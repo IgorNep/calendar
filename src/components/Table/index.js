8f63a8e56e2707b5314db623ce6089549557fbab
IGOR <igornesan@gmail.com>
2021-03-13 20:33:18 +0200

    refactor table render

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,7 +3,7 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-lone-blocks */
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import EventComponent from 'components/EventComponent';
 import { days, time } from 'utils/dataStore';
 import Loader from 'components/common/Loader';
@@ -25,35 +25,23 @@ const Table = ({
   const alertHandler = (message) => {
     showAlert({ message, type: 'danger' });
   };
-  const [eventsData, setEventsData] = useState([]);
   const eventsToRender = filteredEvents || events;
 
-  useEffect(() => {
-    setEventsData(eventsToRender);
-  }, [events, filteredEvents]);
-
   const dragHandler = (item, monitor, fieldId) => {
-    setEventsData((prevState) =>
-      prevState.map((event) => {
-        const findEvent = eventsData.find(
-          (eventItem) => eventItem.fieldId === item.fieldId,
-        );
-        if (findEvent) {
-          findEvent.fieldId = fieldId;
-        }
-        // eslint-disable-next-line no-unused-expressions
-        findEvent && updateEvent(findEvent);
-        return {
-          ...event,
-          event: event.fieldId === item.fieldId ? findEvent : event,
-        };
-      }),
+    const findEvent = events.find(
+      (eventItem) => eventItem.fieldId === item.fieldId,
     );
+    if (findEvent) {
+      findEvent.fieldId = fieldId;
+    }
+    // eslint-disable-next-line no-unused-expressions
+    findEvent && updateEvent(findEvent);
   };
+
   return loading ? (
     <Loader />
   ) : (
-    eventsData && (
+    eventsToRender && (
       <DndProvider backend={HTML5Backend}>
         <table className="table table-bordered">
           <thead>
@@ -75,11 +63,11 @@ const Table = ({
                     key={Math.random() * 100}
                     fieldId={`${dayItem}${hour.substring(0, 2)}`}
                     dragHandler={dragHandler}
-                    events={eventsData}
+                    events={eventsToRender}
                     globalEvents={events}
                     isAdmin={isAdmin}
                   >
-                    {eventsData.map((event) => {
+                    {eventsToRender.map((event) => {
                       if (
                         event.fieldId === `${dayItem}${hour.substring(0, 2)}`
                       ) {
